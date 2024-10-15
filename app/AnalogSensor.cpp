28abae4d79ea258067dc8854d0a132ebf23379e6
Abhey Sharma <abhey2526@gmail.com>
2024-10-15 14:57:48 -0400

    Task#2 Fixed Bugs in main.cpp and AnalogSensor.cpp
    This commit fixes 2 bugs in the code. Initilized variable in main.cpp and fixed memory leak in AnalogSensor by deallocating raw pointer.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -16,6 +16,9 @@ int AnalogSensor::Read()
     std::vector<int> *readings = new std::vector<int>(mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    // Free the allocated memory
+    delete readings;
+
     return result;
 }
 
